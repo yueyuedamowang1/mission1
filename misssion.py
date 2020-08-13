010550b1a50316abd16fa75c523812ec1dc0324f
lanjing247 <1430984357@qq.com>
2020-08-13 13:15:27 +0800

    lanjing

--- misssion.py
+++ misssion.py
@@ -3,21 +3,17 @@ import akshare as ak
 from matplotlib import pyplot as plt
 import seaborn as sns
 def statisticsGet(stockCode,stockRenewed=""):
-   '''stockCode为股票代码，
-      stockRenewed为股票复权情况，
-      该函数返回一个dataframe类型
-   '''
     datatable=ak.stock_zh_a_daily(symbol=stockCode,adjust=stockRenewed)
     datatable=datatable.dropna()
     datatable=datatable.reset_index()
     return  datatable
-def timeGet(DataTable):
-    raw_time = pd.to_datetime(a.pop('date'), format='%Y/%m/%d %H:%M')
+def timeGet(DataTable,ListName='date'):
+    raw_time = pd.to_datetime(DataTable.pop(ListName), format='%Y/%m/%d %H:%M')
     return raw_time
+def plot
  
 a=statisticsGet('sh600418')
 print (a)
-def 
 raw_time=timeGet(a)
 plt.plot(raw_time, a['close'])
 plt.xlabel('Time')
